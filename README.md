5569a4f8d5282f493c44d924560ee1f42d6d8c48
Peter <peathal@yahoo.de>
2013-09-23 16:40:48 +0200

    initial github repo

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Jeannie for Chrome
 
 Try [our demo](https://ask.pannous.com) now!
+
+# License
+
+This project stands under the Apache License 2.0. Of course our logos and images are excluded. We are using for some images [this nice](http://www.famfamfam.com) collection!
+
+# Setup Jeannie for Chrome
+
+TODO describe how to setup this client on localhost
+TODO allow cross origin requests
---
+++ js/common.js
@@ -0,0 +1,96 @@
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function getLocale(l) {
+    return (l == null) ? '' : l.toString();
+}
+
+
+function isDebugging() {
+    if (document.location.hostname == "l" || document.location.hostname == "0.0.0.0") return true;
+    return document.location.hostname == "localhost" || document.location.hostname == "127.0.0.1"
+}
+
+function getTimeZone(tz) {
+    return (tz == null) ? -new Date().getTimezoneOffset() : tz;
+}
+
+function parseUrlWithHisto() {
+    if(window.location.hash)
+        return parseUrl(window.location.hash);
+
+    return parseUrl(window.location.search);
+}
+function parseUrlAndRequest() {
+    return parseUrl(window.location.search);
+}
+
+function parseUrl(query) {
+    var index = query.indexOf('?');
+    if(index >= 0)
+        query = query.substring(index + 1);
+    var res = {};
+    var vars = query.split("&");
+    for (var i=0;i < vars.length;i++) {
+        var tmpVar = vars[i]
+        var equalIndex = tmpVar.indexOf("=")
+        key = tmpVar.substring(0, equalIndex);
+        val = tmpVar.substring(equalIndex + 1);
+        if(val != null)
+            val = decodeURIComponent(val.replace(/\+/g,' '));
+
+        if (typeof res[key] === "undefined")
+            res[key] = val;
+        else if (typeof res[key] === "string") {
+            var arr = [ res[key], val ]; // multi facets
+            res[key] = arr;
+        } else
+            res[key].push(val);
+    }
+    // console.log(res);
+    return res;
+}
+
+function getIsoTime(d){
+    function pad(n){
+        return n<10 ? '0'+n : n
+    }
+    return d.getUTCFullYear()+'-'
+    + pad(d.getUTCMonth()+1)+'-'
+    + pad(d.getUTCDate())+'T'
+    + pad(d.getUTCHours())+':'
+    + pad(d.getUTCMinutes())+':'
+    + pad(d.getUTCSeconds())+'Z'
+}
+
+// quick hack to make tabs working again with history.js
+function prepareTabs(id, link) {
+    // hide all
+    $('#'+id+' div').hide();
+    $('#'+id+' ul li').removeClass('active');
+
+    var contentEl = $('#' + link);
+    var titleEl = $('#'+id+' ul li a[href="#'+link+'"]');
+    if(contentEl.length == 0 || titleEl.length == 0) {
+        // show first content
+        $('#'+id+' div:first').show();
+        // show first title
+        $('#'+id+' ul li:first').addClass('active');
+    } else  {
+        contentEl.show();
+        titleEl.parent().addClass('active');
+    }
+
+    $('#'+id+' ul li a').click(function() {
+        // hide current tab
+        $('#'+id+' ul li').removeClass('active');
+        $('#'+id+' div').hide();
+
+        // show other tab
+        $(this).parent().addClass('active');
+        var currentTab = $($(this).attr('href'));
+        currentTab.show();
+        return false;
+    });
+}
